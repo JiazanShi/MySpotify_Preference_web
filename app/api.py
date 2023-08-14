a9cc9d70f11a9fdc134f4e0560879f5d2a00b0c6
JiazanShi <js12588@nyu.edu>
2023-08-14 16:09:43 -0400

    update api secrets

--- app/api.py
+++ app/api.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 #> invoking this function loads contents of the ".env" file into the script's environment...
 load_dotenv() 
-
+## 
 #to access to spotify api, you need client_id, client_secret
 client_id = os.getenv("CLIENT_ID")
 client_secret =os.getenv("CLIENT_SECRET")
